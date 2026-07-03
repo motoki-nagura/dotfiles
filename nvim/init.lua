13bab9185919240952008bfdc9482c4b1db55f51
名倉元樹 <nagura@MotokinoMacBook-Air.local>
2026-07-03 09:23:10 +0900

    lua snip

--- nvim/init.lua
+++ nvim/init.lua
@@ -186,6 +186,44 @@ require("lazy").setup({
     end,
   },
 
+  -- Snippets: LuaSnip
+  {
+    "L3MON4D3/LuaSnip",
+    version = "v2.*",
+    build = "make install_jsregexp",
+    config = function()
+      local ls = require("luasnip")
+
+      require("luasnip.loaders.from_lua").load({
+        paths = { vim.fn.stdpath("config") .. "/lua/snippets" },
+      })
+
+      local function feedkey(key)
+        vim.api.nvim_feedkeys(
+          vim.api.nvim_replace_termcodes(key, true, true, true),
+          "n",
+          false
+        )
+      end
+
+      vim.keymap.set({ "i", "s" }, "<Tab>", function()
+        if ls.expand_or_jumpable() then
+          ls.expand_or_jump()
+        else
+          feedkey("<Tab>")
+        end
+      end, { silent = true })
+
+      vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
+        if ls.jumpable(-1) then
+          ls.jump(-1)
+        else
+          feedkey("<S-Tab>")
+        end
+      end, { silent = true })
+    end,
+  },
+
   -- Telescope fuzzy finder
   {
     "nvim-telescope/telescope.nvim",
--- nvim/lua/snippets/tex.lua
+++ nvim/lua/snippets/tex.lua
@@ -5,6 +5,7 @@ local t = ls.text_node
 local sn = ls.snippet_node
 local d = ls.dynamic_node
 local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
 local line_begin = require("luasnip.extras.expand_conditions").line_begin
 
 
