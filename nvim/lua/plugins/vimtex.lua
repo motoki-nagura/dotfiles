bb15df2b2cf4789498893e0c71dca11e9a3b3f67
MacBookAir <nagura@MotokinoMacBook-Air.local>
2026-08-03 15:33:37 +0900

    vimtex

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -2,6 +2,14 @@ return {
   "lervag/vimtex",
   init = function()
     vim.g.vimtex_compiler_method = "latexmk"
+
+    -- Use LuaLaTeX as latexmk's default engine.
+    -- <leader>ll (normally \\ll) will compile with latexmk -lualatex.
+    vim.g.vimtex_compiler_latexmk_engines = {
+      _ = "-lualatex",
+    }
+
+    -- macOS PDF viewer with SyncTeX support.
     vim.g.vimtex_view_method = "skim"
   end,
 }
